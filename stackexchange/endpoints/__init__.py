430b53413957278d5b0e6aae080cebb858290785
georgepsarakis <giwrgos.psarakis@gmail.com>
2016-11-06 19:44:37 +0200

    Allow direct imports of endpoint classes

---
+++ stackexchange/endpoints/__init__.py
@@ -0,0 +1,31 @@
+from answers import Answers
+from badges import Badges
+from comments import Comments
+from errors import Errors
+from events import Events
+from info import Info
+from posts import Posts
+from privileges import Privileges
+from questions import Questions
+from revisions import Revisions
+from search import Search
+from sites import Sites
+from tags import Tags
+from users import Users
+
+__all__ = [
+    'Answers',
+    'Badges',
+    'Comments',
+    'Errors',
+    'Events',
+    'Info',
+    'Posts',
+    'Privileges',
+    'Questions',
+    'Revisions',
+    'Search',
+    'Sites',
+    'Tags',
+    'Users',
+]
