e8d71870e8377fde9efc80cfa6b71d1b457eb9da
georgepsarakis <giwrgos.psarakis@gmail.com>
2016-11-06 19:49:29 +0200

    Python3: Convert to relative import

--- stackexchange/endpoints/__init__.py
+++ stackexchange/endpoints/__init__.py
@@ -1,17 +1,17 @@
-from answers import Answers
-from badges import Badges
-from comments import Comments
-from errors import Errors
-from events import Events
-from info import Info
-from posts import Posts
-from privileges import Privileges
-from questions import Questions
-from revisions import Revisions
-from search import Search
-from sites import Sites
-from tags import Tags
-from users import Users
+from .answers import Answers
+from .badges import Badges
+from .comments import Comments
+from .errors import Errors
+from .events import Events
+from .info import Info
+from .posts import Posts
+from .privileges import Privileges
+from .questions import Questions
+from .revisions import Revisions
+from .search import Search
+from .sites import Sites
+from .tags import Tags
+from .users import Users
 
 __all__ = [
     'Answers',
